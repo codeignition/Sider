3006354f02fa551d73f75c1969776ae3712a497b
Sinister Light <me@sinisterlight.com>
2014-06-19 01:12:49 +0530

    [Shobhit|Deba|Hari] fixing database model

--- app/models/database.server.model.js
+++ app/models/database.server.model.js
@@ -4,34 +4,35 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
-	Schema = mongoose.Schema;
+Schema = mongoose.Schema;
 
 /**
  * Database Schema
  */
 var DatabaseSchema = new Schema({
-	name: {
-		type: String,
-		default: '',
-		required: 'Please fill Database name',
-		trim: true
-	},
-    	host: {
-    		type: String,
-    		required: 'Please provide host ip to your Redis database'
-    	},
-    	port: {
-    	type : String,
-    	required : 'Please provide port'
-    	},
-    	created: {
-		type: Date,
-		default: Date.now
-	},
-	user: {
-		type: Schema.ObjectId,
-		ref: 'User'
-	}
+  name: {
+    type: String,
+    default: '',
+    required: 'Please fill Database name',
+    trim: true
+  },
+  host: {
+    type: String,
+    required: 'Please provide host ip to your Redis database'
+  },
+  port: {
+    type : Number,
+    default: 6379,
+    required : 'Please provide port'
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  user: {
+    type: Schema.ObjectId,
+    ref: 'User'
+  }
 });
 
 mongoose.model('Database', DatabaseSchema);
--- app/tests/database.server.model.test.js
+++ app/tests/database.server.model.test.js
@@ -4,9 +4,9 @@
  * Module dependencies.
  */
 var should = require('should'),
-	mongoose = require('mongoose'),
-	User = mongoose.model('User'),
-	Database = mongoose.model('Database');
+mongoose = require('mongoose'),
+User = mongoose.model('User'),
+Database = mongoose.model('Database');
 
 /**
  * Globals
@@ -17,89 +17,68 @@ var user, database;
  * Unit tests
  */
 describe('Database Model Unit Tests:', function() {
-	beforeEach(function(done) {
-		user = new User({
-			firstName: 'Full',
-			lastName: 'Name',
-			displayName: 'Full Name',
-			email: 'test@test.com',
-			username: 'username',
-			password: 'password'
-		});
+  beforeEach(function(done) {
+    user = new User({
+      firstName: 'Full',
+      lastName: 'Name',
+      displayName: 'Full Name',
+      email: 'test@test.com',
+      username: 'username',
+      password: 'password'
+    });
 
-		user.save(function() { 
-			database = new Database({
-				name: 'Database Name',
-				 host:'host',
-				 port:'port',
-				user: user
-			});
+    user.save(function() { 
+      database = new Database({
+        name: 'Database Name',
+        host:'host',
+        port: 1000,
+        user: user
+      });
 
-			done();
-		});
-	});
+      done();
+    });
+  });
 
-	describe('Method Save', function() {
-		it('should be able to save without problems', function(done) {
-			return database.save(function(err) {
-				should.not.exist(err);
-				done();
-			});
-		});
+  describe('Method Save', function() {
+    it('should be able to save without problems', function(done) {
+      return database.save(function(err) {
+        should.not.exist(err);
+        done();
+      });
+    });
 
-		it('should be able to show an error when try to save without name', function(done) { 
-			database.name = '';
+    it('should be able to show an error when try to save without name', function(done) { 
+      database.name = '';
 
-			return database.save(function(err) {
-				should.exist(err);
-				done();
-			});
-		});
-	it('should be able to show an error when try to save without host', function(done) { 
-			database.host = '';
+      return database.save(function(err) {
+        should.exist(err);
+        done();
+      });
+    });
 
-			return database.save(function(err) {
-				should.exist(err);
-				done();
-			});
-		});
-it('should be able to show an error when try to save without port', function(done) { 
-			database.port = '';
+    it('should be able to show an error when try to save without host', function(done) { 
+      database.host = '';
 
-			return database.save(function(err) {
-				should.exist(err);
-				done();
-			});
-		});
-	});
-	describe('Method Read', function() {
-		//creating a sample user
-		sampleuser = new User({
-			firstName: 'sampleFull',
-			lastName: 'sampleName',
-			displayName: 'sampleFull sampleName',
-			email: 'sampletest@test.com',
-			username: 'sampleusername',
-			password: 'samplepassword'
-		});
-			
-		sampleuser.save(function() { 
-			done();
-		});
+      return database.save(function(err) {
+        should.exist(err);
+        done();
+      });
+    });
 
+    it('should be able to show an error when try to save without port', function(done) { 
+      database.port = '';
 
-		it('other user cant read other user stuff', function(done) {
-			return database.save(function(err) {
-				should.not.exist(err);
-				done();
-			});
-		});
-          });
+      return database.save(function(err) {
+        should.exist(err);
+        done();
+      });
+    });
+  });
 
-	afterEach(function(done) { 
-		Database.remove().exec();
-		User.remove().exec();
+  afterEach(function(done) { 
+    Database.remove().exec();
+    User.remove().exec();
 
-		done();
-	});
+    done();
+  });
 });
