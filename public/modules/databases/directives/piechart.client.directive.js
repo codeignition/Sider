ab977a0cbe9b8fd82d6f8227a70f62e7b6c2684d
hogaur <hogaur@gmail.com>
2014-06-26 13:59:46 +0530

    [Shobhit|Deba|Hari] Displays and removes pie chart successfully

--- public/modules/databases/directives/piechart.client.directive.js
+++ public/modules/databases/directives/piechart.client.directive.js
@@ -2,26 +2,24 @@
 
 angular.module('databases')
 .directive('piechart',
-           function(){
-             var directive = {};
-             directive.restrict = 'E';
-             directive.scope = {
-               info: '=info'
-             }
-             
-            directive.transclude = true;
-             //directive.template='<p>here i am {{info}}</p>';
-             //console.log(directive.scope.info);
-             
-             directive.link = function (scope, element, attrs) {
-               scope.$watch( function(){
-                 var r = Raphael(200,300,800,800);
-               // console.log(scope.info);
-                 r.piechart(100,100,100,scope.info); //, scope.info.db1.keys]);
-               });
-             }
+	function(){
+		var directive = {};
+		directive.restrict = 'E';
+		directive.scope = {
+			info: '=info'
+		}
+		var a=100; 
+		directive.link = function (scope, element, attrs) {
+			element = Raphael(200,300,800,800);
+			scope.$watch( function(){
+				element.piechart(100,100,a,scope.info); //, scope.info.db1.keys]);
+			});
+			scope.$on('$destroy',function(){
+				element.remove();
+			});	
+		}
 
-             return directive;
+		return directive;
 
-           }
-          );
+	}
+);
