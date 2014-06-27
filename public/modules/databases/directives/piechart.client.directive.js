be457a031b5957deebfc075a9a32291b9d141b9e
wallydrag <debashish.ghatak@gmail.com>
2014-06-27 11:35:10 +0530

    [Deba] resolving piechart issue-1, pie chart only showing on view page now

--- public/modules/databases/directives/piechart.client.directive.js
+++ public/modules/databases/directives/piechart.client.directive.js
@@ -2,28 +2,26 @@
 
 angular.module('databases')
 .directive('piechart',
-	function(){
-		var directive = {};
-		directive.restrict = 'E';
-		directive.scope = {
-			info: '=info',
-      names:'=names'
-		}
-		var a=100;
-		directive.link = function (scope, element, attrs) {
-      console.log(scope.info);
-      //x=x/2;
-			scope.$watch( function(){
-      var x = element.children(0)[0].clientWidth;
-      var ele = Raphael(x/2,x/2,2*x,2*x);
-				ele.piechart(x/4,x/4,x/4,scope.info,{ legend:scope.names}); //, scope.info.db1.keys]);
-			});
-			scope.$on('$destroy',function(){
-				ele.remove();
-			});
-		}
+           function(){
+             var directive = {};
+             directive.restrict = 'E';
+             directive.scope = {
+               info: '=info',
+               names:'=names'
+             }
+             var a=100;
+             directive.link = function (scope, element, attrs) {
+               scope.$watch( function(){
+                 var x = element.children(0)[0].clientWidth;
+                 var ele = Raphael(x/2,x/2,2*x,2*x);
+                 ele.piechart(x/4,x/4,x/4,scope.info,{ legend:scope.names});
+                 scope.$on('$destroy',function(){
+                   ele.remove();
+                 });
+               });
+             }
 
-		return directive;
+             return directive;
 
-	}
-);
+           }
+          );
