915f6c890d767e0b96b87046a02eeed36c9c86d4
wallydrag <debashish.ghatak@gmail.com>
2014-06-30 14:02:00 +0530

    [Deba] initial model unit test of info

--- tests/models/info.server.model.test.js
+++ tests/models/info.server.model.test.js
@@ -4,52 +4,63 @@
  * Module dependencies.
  */
 var should = require('should'),
-	mongoose = require('mongoose'),
-	User = mongoose.model('User'),
-	Info = mongoose.model('Info');
+mongoose = require('mongoose'),
+User = mongoose.model('User'),
+Database = mongoose.model('Database'),
+Info = mongoose.model('Info');
 
 /**
  * Globals
  */
-var user, info;
+var user, info, database;
 
 /**
  * Unit tests
  */
 describe('Info Model Unit Tests:', function() {
-	beforeEach(function(done) {
-		user = new User({
-			firstName: 'Full',
-			lastName: 'Name',
-			displayName: 'Full Name',
-			email: 'test@test.com',
-			username: 'username',
-			password: 'password'
-		});
+  beforeEach(function(done) {
+    user = new User({
+      firstName: 'Full',
+      lastName: 'Name',
+      displayName: 'Full Name',
+      email: 'test@test.com',
+      username: 'username',
+      password: 'password'
+    });
 
-		user.save(function() { 
-			info = new Info({
-				// Add model fields
-				// ...
-			});
+    user.save(function() {
+      database = new Database({
+        name: 'Database Name',
+        host: 'host',
+        port: 1000,
+        user: user
+      });
+      database.save(function(){
+        info = new Info({
+        database: database,
+        content: { redis_version: '2.6.16',
+          redis_git_sha1: '00000000',
+          versions: [ 2, 6, 16 ] }
+        });
+      });
+    });
+      done();
+    });
 
-			done();
-		});
-	});
 
-	describe('Method Save', function() {
-		it('should be able to save without problems', function(done) {
-			return info.save(function(err) {
-				should.not.exist(err);
-				done();
-			});
-		});
-	});
+  describe('Method Save', function() {
+    it('should be able to save without problems', function(done) {
+      return info.save(function(err) {
+        should.not.exist(err);
+        done();
+      });
+    });
+  });
 
-	afterEach(function(done) { 
-		Info.remove().exec();
-		User.remove().exec();
+  afterEach(function(done) {
+    Info.remove().exec();
+    User.remove().exec();
 
-		done();
-	});
+    done();
+  });
 });
