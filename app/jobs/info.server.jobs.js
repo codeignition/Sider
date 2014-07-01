1be316345477dc6979932c82fe7e7f339e29ed9b
wallydrag <debashish.ghatak@gmail.com>
2014-07-01 12:44:19 +0530

    [Deba] writing exports.job with event emitter

--- app/jobs/info.server.jobs.js
+++ app/jobs/info.server.jobs.js
@@ -1,6 +1,33 @@
 'use strict';
-var schedule = require('node-schedule');
-schedule.scheduleJob('* * * * * *', function(){
-      console.log('The answer to life, the universe, and everything!');
-  console.log(Date.now);
-});
+var redis = require('redis');
+var mongoose = require('mongoose');
+var database = mongoose.model('Database');
+var Info = mongoose.model('Info');
+var dbs, client, info, serverinfo;
+var events = require('events');
+var async = require('async');
+var client, redisinfo, serverinfo, dbs, db;
+var EventEmitter = events.EventEmitter;
+exports.infoEmitter = new EventEmitter();
+
+exports.job = function(){
+  database.find()
+  .exec(function(err, dbs){
+    async.each(dbs, function(db, callback){
+      client = redis.createClient(db.port, db.host);
+      client.info(function(err, redisinfo){
+        var info = new Info({
+          database : db,
+          content : client.server_info
+        });
+        client.quit();
+        info.save(function(err){
+          callback();
+        });
+      });
+    },function(err){
+      if (err) console.log(err);
+      exports.infoEmitter.emit('jobdone');
+    });
+  });
+}
