56a7331828f760c0632497ecbccb01ac02dc8690
wallydrag <debashish.ghatak@gmail.com>
2014-07-01 12:46:56 +0530

    [Deba] test for custom jobs function

---
+++ tests/jobs/info.server.jobs.test.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var redis = require('redis');
+var Database = mongoose.model('Database');
+var Info = mongoose.model('Info');
+var jobs = require('../../app/jobs/info.server.jobs.js');
+var should = require('should');
+var count;
+describe('Jobs test', function(){
+  beforeEach(function(done){
+    var database1 = new Database({
+      name:'name',
+      port: 6379,
+      host: 'localhost'
+    });
+    var database2 = new Database({
+      name:'name',
+      port: 6379,
+      host: 'localhost'
+    });
+    database1.save(function(){
+      database2.save(function(){
+        done();
+      });
+    });
+  });
+
+  it('should be able to return redis-info', function(done){
+    jobs.job();
+    jobs.infoEmitter.on('jobdone', function(){
+      Info.count({}, function(err, count){
+        count.should.equal(2);
+        done();
+      });
+    });
+  });
+
+  afterEach(function(done){
+    Database.remove().exec();
+    done();
+  });
+});
