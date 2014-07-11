7b5d2ece51ca697032a219ca499abfd96ba410de
wallydrag <debashish.ghatak@gmail.com>
2014-07-11 13:46:55 +0530

    [Deba] issue #23 partly resolved, description in my comment in issue thread

--- public/modules/databases/directives/linechart.client.directive.js
+++ public/modules/databases/directives/linechart.client.directive.js
@@ -4,10 +4,11 @@ angular.module('databases')
 .directive('linechart', [
   function() {
   var directive = {};
-  directive.restrict = 'E';
+  directive.restrict = 'AE';
   directive.scope = {
     mem:'=mem',
-    time:'=time'
+    time:'=time',
+    xmarking:'=xcord'
   };
 
   directive.link = function(scope, element, attrs){
@@ -17,10 +18,11 @@ angular.module('databases')
     scope.$watch( function(){
       var timeArray=scope.time;
       var memArray=scope.mem;
-      if(timeArray&&memArray&&entry){
+      if(timeArray&&memArray&&entry&&scope.xmarking){
         entry=false;
-        var linePaper = Raphael('line');
-        linePaper.linechart(50,10,300,300, timeArray, memArray,{axis:'0 0 1 1', symbol: 'circle', smooth:false});
+        var linePaper = Raphael(element[0]);
+        linePaper.linechart(50,10,630,300, timeArray, memArray,{axis:'0 0 0 1', symbol: 'circle', smooth:false});
+        Raphael.g.axis(60, 300, 610, null, null, timeArray.length-1, 0, scope.xmarking, linePaper);
       };
       scope.$on('$destroy', function(){
         linePaper.remove();
